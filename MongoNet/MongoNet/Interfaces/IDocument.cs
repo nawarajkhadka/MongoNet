59027f70efb5f7d63efb135ff7328fe769e0ccee
Nawaraj Khadka <nawarajkhadka91@gmail.com>
2025-04-02 13:17:27 +0200

    initial files added

---
+++ MongoNet/MongoNet/Interfaces/IDocument.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+
+namespace MongoNet.Interfaces
+{
+    public interface IDocument
+    {
+        ObjectId Id { get; set; }
+
+        DateTime CreatedAt { get; }
+    }
+}
+
+﻿
---
+++ MongoNet/MongoNet/Interfaces/IMongoNetRepo.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+
+namespace MongoNet.Interfaces
+{
+    public interface IMongoNetRepo<TEntity>
+    {
+        Task Create(TEntity document);
+        Task<TEntity> Replace(string id, TEntity newDocument);
+        Task<bool> Delete(string id);
+        Task<TEntity> Get(Expression<Func<TEntity, bool>> filterExpression);
+        Task<TEntity> GetById(string id);
+        Task<IEnumerable<TEntity>> Get();
+        Task<IEnumerable<TEntity>> GetTop(int limit);
+        IEnumerable<TProjected> FilterBy<TProjected>(Expression<Func<TEntity, bool>> filterExpression, Expression<Func<TEntity, TProjected>> projectionExpression);
+        Task<bool> DeleteAllDocuments();
+        Task<IEnumerable<TEntity>> FilterBy(Expression<Func<TEntity, bool>> filterExpression);
+        IQueryable<TEntity> GetAsQuerable();
+        IEnumerable<TEntity> GetTextSearchResults(string index, string searchField, string searchText);
+        IEnumerable<TEntity> GetRegexSearchResults(string searchField, string regex, string index);
+
+    }
+}
+
